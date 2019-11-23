759c98442a2a0afade900879d2cbd9dbdb0f5e5c
logicalway <info@logicalway.ch>
2019-11-23 11:17:35 +0100

    ver 4.1.0
    init

---
+++ themes/simple/views/user/userhome.ctp.php
@@ -0,0 +1,7 @@
+<div class="col-sm-12"> 
+    <div id="content">
+    	<script type="text/javascript">
+           	scriptDoLoad('archive.php', 'content', '<?php echo getRequestParamStr(); ?>');
+    	</script>
+    </div>
+</div>
